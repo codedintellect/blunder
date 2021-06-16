df896e3ece8eb051deaab4490f284f54ea0252c1
Dmitri <codedintellect@gmail.com>
2021-06-16 17:52:37 +0300

    Session saving

--- public/auth/script.js
+++ public/auth/script.js
@@ -3,27 +3,32 @@ function submit() {
   var email = $("#email").val();
   var password = $("#pass").val();
   if (params == "login") {
-    firebase.auth().signInWithEmailAndPassword(email, password)
-      .then((userCredential) => {
-        var user = userCredential.user;
-        alert("LOGINNED");
+    firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL)
+      .then(() => {
+        return firebase.auth().signInWithEmailAndPassword(email, password);
       })
       .catch((error) => {
         var errorCode = error.code;
         var errorMessage = error.message;
-        alert(errorMessage);
+        console.log(error.message);
       });
   }
   else if (params == "register") {
     firebase.auth().createUserWithEmailAndPassword(email, password)
       .then((userCredential) => {
         var user = userCredential.user;
-        alert("REGISTERED");
+
       })
       .catch((error) => {
         var errorCode = error.code;
         var errorMessage = error.message;
-        alert(errorMessage);
+
       });
   }
 }
+
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    window.location.replace("/")
+  }
+});
