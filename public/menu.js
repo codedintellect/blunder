b078d52953909be1ca59d55c372e7ea1867b73d5
DmitrievPlays <dmitriev8925@gmail.com>
2021-06-17 18:06:58 +0300

    UI Changes...

--- public/menu.js
+++ public/menu.js
@@ -1,32 +1,15 @@
 $(document).ready(function() {
 
   $(".user_tile").click(
-    function menuOpen() {
-    if(  $("#user_dropdown").css("display") == "none")
-      $("#user_dropdown").css("display", "flex");
-    else
-      $("#user_dropdown").css("display", "none");
+    function a() {
+      $("#profile").show();
+      $(".profile_bg").show();
   });
 
-
-  $("#settings").click(
-    function a() {
-      $("#settings_window").show();
-      $(".settings_window_bg").show();
-    }
-  )
-
-  $(".settings_window_close").click(
+  $(".profile_close").click(
     function b() {
-      $("#settings_window").hide();
-      $(".settings_window_bg").hide();
+      $("#profile").hide();
+      $(".profile_bg").hide();
     }
   )
-});
-
-
-$(document).click(function(event) {
-  var $target = $(event.target);
-  if(!$target.closest(".user_tile").length)
-    $("#user_dropdown").css("display", "none");
 });
