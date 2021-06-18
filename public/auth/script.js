5d8cd2d7970860bae6c3876feb15638b8a9d0472
Dmitri <codedintellect@gmail.com>
2021-06-18 13:16:45 +0300

    Fixed auth saving

--- public/auth/script.js
+++ public/auth/script.js
@@ -3,26 +3,23 @@ function submit() {
   var email = $("#email").val();
   var password = $("#pass").val();
   if (params == "login") {
-    firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL)
-      .then(() => {
-        return firebase.auth().signInWithEmailAndPassword(email, password);
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        var user = userCredential.user;
       })
       .catch((error) => {
         var errorCode = error.code;
         var errorMessage = error.message;
-        console.log(error.message);
       });
   }
   else if (params == "register") {
     firebase.auth().createUserWithEmailAndPassword(email, password)
       .then((userCredential) => {
         var user = userCredential.user;
-
       })
       .catch((error) => {
         var errorCode = error.code;
         var errorMessage = error.message;
-
       });
   }
 }
