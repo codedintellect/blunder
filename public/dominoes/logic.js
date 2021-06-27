d08e5a8cd6b8f51fb9a9642cb59323acb300e5b5
DmitrievPlays <dmitriev8925@gmail.com>
2021-06-27 19:42:18 +0300

    God, save us all... Dominoes are here!

--- public/dominoes/logic.js
+++ public/dominoes/logic.js
@@ -1,5 +1,6 @@
 var b = $("#board");
-var ctx = b[0].getContext("2d")
+var ctx = b[0].getContext("2d");
+var p;
 
 var mouseX, mouseY, lastX, lastY;
 
@@ -11,22 +12,53 @@ b.mousemove(function(event) {
   drawer(event.buttons == 1);
 });
 
-function drawer(drag) {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  var p = new Path2D("M0 0 h 100 v 100 h -100 z");
-  ctx.fillStyle = "#000000";
+function setup() {
+  //p = new Path2D("M0 0 h 60 v 120 h -60 z");
+  ctx.fillStyle = "#ffffff";
+  //ctx.fill(p);
+  p = new Path2D;
+  p.beginPath
+  p.lineTo(50, 0);
+  p.bezierCurveTo(50, 0, 60, 0, 60, 10);
+  p.lineTo(60, 110);
+  p.bezierCurveTo(60, 110, 60, 120, 50, 120);
+  p.lineTo(10, 120);
+  p.bezierCurveTo(10, 120, 0, 120, 0, 110);
+  p.lineTo(0, 10);
+  p.bezierCurveTo(0, 10, 0, 0, 10, 0);
+  p.closePath;
+
   ctx.fill(p);
-  if (ctx.isPointInPath(p, lastX, lastY) && drag) {
+}
 
+function drawer(drag) {
+
+  if (ctx.isPointInPath(p, lastX, lastY) && drag) {
+    p = new Path2D;
+    //p = new Path2D("M" + (mouseX-30) + " " + (mouseY-60) + " h 60 v 120 h -60 z");
+
+    p.beginPath;
+    p.moveTo(mouseX + 50, mouseY + 0);
+    p.bezierCurveTo(mouseX + 50, mouseY + 0, mouseX + 60, mouseY + 0, mouseX + 60, mouseY + 10);
+    p.lineTo(mouseX + 60, mouseY + 110);
+    p.bezierCurveTo(mouseX + 60, mouseY + 110, mouseX + 60, mouseY + 120, mouseX + 50, mouseY + 120);
+    p.lineTo(mouseX + 10, mouseY + 120);
+    p.bezierCurveTo(mouseX + 10, mouseY + 120, mouseX + 0, mouseY + 120, mouseX + 0, mouseY + 110);
+    p.lineTo(mouseX + 0, mouseY + 10);
+    p.bezierCurveTo(mouseX + 0, mouseY + 10, mouseX + 0, mouseY + 0, mouseX + 10, mouseY + 0);
+    p.closePath;
+
+    ctx.clearRect(0, 0, b.width(), b.height());
+    ctx.fill(p);
   }
 }
-drawer(false);
 
-document.getElementById('clickme').onclick = function clickEvent(e) {
-      // e = Mouse click event.
-      var rect = e.target.getBoundingClientRect();
-      var x = e.clientX - rect.left; //x position within the element.
-      var y = e.clientY - rect.top;  //y position within the element.
-      console.log("Left? : " + x + " ; Top? : " + y + ".");
-    }
+document.getElementById('board').onclick = function clickEvent(e) {
+  var rect = e.target.getBoundingClientRect();
+  var x = e.clientX - rect.left;
+  var y = e.clientY - rect.top;
+  console.log("Left? : " + x + " ; Top? : " + y + ".");
+}
+
+setup();
