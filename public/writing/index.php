ae58653988365f632ef6e16e55345022a424b056
DmitrievPlays <dmitriev8925@gmail.com>
2021-07-28 23:04:03 +0300

    *Sighs*

--- public/writing/index.php
+++ public/writing/index.php
@@ -8,15 +8,42 @@
     <link rel="stylesheet" href="style.css"/>
   </head>
   <body>
-    <div id="message">Теперь Ваш друг %DISPLAY_NAME% придумывает слова к Вашим предложениям! Подождите немного...</div>
-    <div id="game">
+    <div id="message">Ваш ответ принят. Теперь ждём ответ Вашего друга - %DISPLAY_NAME%</div>
+    <div id="game" class="start" style="width: 400px; height: auto; display: block; background-color: #292e3a;">
+      <div style="position: relative; padding: 0 0 10px 0; text-align: center; font-size: 36px; font-weight: bold; color: #ffffff;">Режим игры</div>
+      <div style="position: relative; display: flex; flex-direction: column;">
+        <button style="margin: 10px 0;" onclick="sentencesClick()">СОСТАВИТЬ ПРЕДЛОЖЕНИЯ</button>
+        <button style="margin: 10px 0;" onclick="wordsClick()">ПОДОБРАТЬ СЛОВА</button>
+      </div>
+    </div>
+
+    <div id="game" class="sentences" style="display: none;">
       <div style="width: 50%; margin: 0 10px; position: relative; float: left;">
-        <div class="task">Напишите несколько предложений с пропусками некоторых слов (на месте пропущенных слов пишите "...") и нажмите "Продолжить", когда будете готовы.</div>
+        <div class="task">Напишите несколько предложений с пропусками некоторых слов (на месте пропущенных слов пишите "---") и нажмите "Продолжить", когда будете готовы.</div>
+        <div id="list" class="sentencesList">
+          <div class="input-buffer" style="position: absolute; visibility: hidden; white-space: nowrap;"></div>
+          <textarea class="textInput">Это был прекрасный ---! Ничто не могло его испортить. Разве что --- и ---</textarea>
+        </div>
+        <button onclick="messageOut()" style="width: 100%; top: 10px;">ПРОДОЛЖИТЬ</button>
+      </div>
+
+      <div style="width: 50%; margin: 0 10px; position: relative; float: right;">
+        <div class="task">И вот что у вас получилось:</div>
         <div id="list">
           <div class="input-buffer" style="position: absolute; visibility: hidden; white-space: nowrap;"></div>
-          <textarea class="textInput">Это был прекрасный ...! Ничто не могло его испортить. Разве что ... и ...</textarea>
+          <p class="test">UwU</p>
         </div>
-        <button onclick="messageOut()">ПРОДОЛЖИТЬ</button>
+      </div>
+    </div>
+
+    <div id="game" class="words" style="display: none;">
+      <div style="width: 50%; margin: 0 10px; position: relative; float: left;">
+        <div class="task">Напишите несколько слов и нажмите "Продолжить", когда будете готовы.</div>
+        <div id="list" class="wordsList">
+          <div class="input-buffer" style="position: absolute; visibility: hidden; white-space: nowrap;"></div>
+          <!--input class="d"style="background-color: #73756d; font-size: 28px; width: 500px;"></input-->
+        </div>
+        <button onclick="messageOut()" style="width: 100%; top: 10px;">ПРОДОЛЖИТЬ</button>
       </div>
 
       <div style="width: 50%; margin: 0 10px; position: relative; float: right;">
--- public/writing/logic.js
+++ public/writing/logic.js
@@ -1,5 +1,14 @@
-$(document).ready(
-  function generate() {
+var text = "Он шел в %с. День был %п и %п. Ничто не могло испортить его, разве что %с.";
+var noun = [];
+var adject = [];
+var verb = [];
+var adverb = [];
+var c, adj, v, adv = 0;
+
+$(document).ready(function a() {
+   wordsInput();
+}
+  /*function generate() {
     var $input = $("input"),
     $buffer = $(".input-buffer");
 
@@ -9,16 +18,65 @@ $(document).ready(
         $(this).width($buffer.width());
     });
   })
-  }
+}*/
+
 )
 
 function messageOut() {
-  $("#message").css({"transform":"translateY(0%)", "top": "10px", "visibility": "visible"});
+  $("#message").css({"transform":"translate(-50%, 0%)", "top": "10px", "visibility": "visible"});
   textConvert();
+  saveInputs();
+
+  setTimeout(messageIn, 4000);
+}
+
+function messageIn() {
+  $("#message").css({"transform":"translate(-50%, -100%)", "top": "0px", "visibility": "hidden"});
+}
+
+
+
+function wordsInput() {
+  c = (text.match(/\%с/g) || []).length;
+  adj = (text.match(/\%п/g) || []).length;
+  v = (text.match(/\%г/g) || []).length;
+  adv = (text.match(/\%н/g) || []).length;
+
+  if(c != 0) $(".wordsList").append("<p>Существительные:</p>")
+  for (var i = 0; i < c; i++)
+    $(".wordsList").append("<input></input>")
+
+  if(adj != 0) $(".wordsList").append("<p>Прилагательные:</p>")
+  for (var i = 0; i < adj; i++)
+    $(".wordsList").append("<input></input>")
+
+  if(v != 0) $(".wordsList").append("<p>Глаголы:</p>")
+  for (var i = 0; i < v; i++)
+    $(".wordsList").append("<input></input>")
+
+  if(adv != 0) $(".wordsList").append("<p>Наречия:</p>")
+  for (var i = 0; i < adv; i++)
+    $(".wordsList").append("<input></input>")
+}
+
+function saveInputs() {
+  $("input").each(function(index) {
+      noun.push($(this).val());   // DOESN'T WORK NORMALLY!
+    })
+  alert(noun);
 }
 
 function textConvert() {
   var text = $(".textInput").val();
-  var newtext = text.replaceAll("...", "<input></input>");
-  $(".test").html(newtext);
+  var newtext = text.replaceAll("---", "<input></input>");
+}
+
+function sentencesClick() {
+  $(".sentences").css("display", "flex");
+  $(".start").css("display", "none");
+}
+
+function wordsClick() {
+  $(".words").css("display", "flex");
+  $(".start").css("display", "none");
 }
